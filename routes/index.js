d0931490a2cc246eebd34449554f55082e8eeafd
val rigodon <rvrigodon@gmail.com>
2017-01-26 16:06:10 -0500

    fazbook app

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/users');
+    res.redirect('/users');
 });
+
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../server/models/index');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('users/index', { title: 'fazbook' });
+    res.render('users/index', { title: 'fazbook' });
+});
+router.get('/new', function(req, res, next) {
+    res.render('users/new', { title: 'make an account' });
+});
+
+router.post('/', function(req, res, next) {
+    models.User.create({
+        firstName: req.body.firstName,
+        lastName: email: dob: req.body.lastname,
+        email: req.body.email,
+        dob: req.body.dob
+    }).then(function() {
+        res.redirect('/users')
+    })
+});
+
+router.get('/', function(req, res, next) {
+    models.User.findAll({}).then(function(users) {
+        res.render('users/index', {
+            title: 'fazbook',
+            users: users
+        });
+    });
 });
 
 module.exports = router;
